9d8bc89283bb6aa647887859cb2fedb6123d5928
Luis Medina <medinaluisgerardo@gmail.com>
2021-09-05 21:43:00 -0500

    Driver
    updated python driver access

--- vitae.py
+++ vitae.py
@@ -1,10 +1,18 @@
 import sys
 from linkedin_scraper import Person, actions
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 from Persone import Persone
 
-driver = webdriver.Chrome('/usr/local/bin/chromedriver')
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
+
+driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=chrome_options)
 
 email = sys.argv[1]
 password = sys.argv[2]
