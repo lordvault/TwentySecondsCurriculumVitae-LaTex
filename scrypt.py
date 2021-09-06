574be99169666f643bf35f7e479ae497f3f9ec80
Luis Medina <medinaluisgerardo@gmail.com>
2021-09-05 20:38:23 -0500

    Secrets
    Added some secrets

--- scrypt.py
+++ scrypt.py
@@ -1,13 +1,6 @@
-from linkedin_api import Linkedin
+import os
 
-# Authenticate using any Linkedin account credentials
-api = Linkedin('reedhoffman@linkedin.com', '*******')
 
-# GET a profile
-profile = api.get_profile('billy-g')
+secret = os.getenv('SECRET_KEY')
 
-# GET a profiles contact info
-contact_info = api.get_profile_contact_info('billy-g')
-
-# GET 1st degree connections of a given profile
-connections = api.get_profile_connections('1234asc12304')
+print(f"This is a test for secrets {secret}")
