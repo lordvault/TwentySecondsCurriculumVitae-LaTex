b36caf44d52a5ebe75771c09d26b624a23f1249a
Luis Medina <medinaluisgerardo@gmail.com>
2021-09-09 18:58:15 -0500

    Resolution
    Added resolution to avoid about issues

--- vitae.py
+++ vitae.py
@@ -11,6 +11,7 @@ def scrape_linked_in_info():
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument("--window-size=1024,1366")
     chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_options.add_experimental_option('useAutomationExtension', False)
 
